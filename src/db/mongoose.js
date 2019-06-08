bce9824767f9cb437a311c51541182750134bfbe
gabrielsxp <gabrielsxp@hotmail.com>
2019-06-08 16:46:18 -0300

    Add files via upload

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 	Código responsável por definir a string de conexão com o MongoDB
 */
 
-var mongoURL = 'mongodb://ec2-3-219-12-16.compute-1.amazonaws.com:27017';
+var mongoURL = 'mongodb://ec2-52-70-90-53.compute-1.amazonaws.com:27017/building-complex-api';
 console.log(mongoURL);
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
